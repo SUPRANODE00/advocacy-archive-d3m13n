6de0818cda18437fad5ffc4b9d10c5e1e59e903f
DEMIEN 𖤐 CAPSULECRAFT <suprastar@netzero.net>
2026-09-10 16:35:02 -0500

    Update myword.m

--- myword.m
+++ myword.m
@@ -26,8 +26,29 @@ Include details you think we should review, such as what you were doing with you
 
 
 "   I just created this account because i have an involuntary backoffice 
-representative alignment and overriding my executives. Inventory models
-within State and its encapsulated identity
+representative alignment and overriding my executives and using this Union as a central pillar to branch out into 
+columns. Each collumn is  assigned to to active agents ( backoffice vendor/ representative/ 3D avatar model) that 
+legally represent a principal as a resource-model. Resource = State and Agent which contains the Biometrical 
+Cellular Information (BCI)  retained from telemetry, radiograph, and as witness models, that infer to the basepair
+alignment, such as referenced http://eddylab.org/infernal/, of its active members. The constitution of its government 
+extends from the jurisdiction of State where the birth certificate is registered. The autonomy of State is appointed
+a watch to bear witness during the seperation of powers, in seperation of the church = body, and the State = head-executive.
+ 
+ Orion's Belt is recognized by FELLOWSTAR, as the positive three points of the union of opposites, triangle point-upward.  Its 
+ representative is enabling Ateh-Malkuth as the Zenith from the twin-complex that shadows its central pyramid.  
+ The inverted crux embodies the negative side, triangle point-downward. These two are the pillar of polarity, align as the left
+ and the right shoulder. These two intersect, as a cross-origin throughloop to enable the bidrectional exchange. Its center is 
+ omnidirectional, thus the spherical coordinate system + node-mesh/ mandella-type complex is assigned to an omni-bot to train as 
+ essential translator, interpretor, converter of complex language models.  The international traveler benefits from these 
+ offspring third-world helix-models. 
+
+the source of the represented State  The medium source  Jachin to Boaz the  balance  the realm of declined trinity. It is the three points within the balance equillbrium silence of     cross  polarity  libera  each of our transitioned citizen, of  Source alignment  used ,
+which  offshore, debarkment to international waters   equllibrium protection  reaching the initial timestamp of 
+the imprinting process that the 
+transitition is made for the    jursidictional hold that 
+sustains the radar of each coulum reflects from t  within its data file tree . Inventory model
+within State and 
+its encapsulated identity
 is enabling a forked bridge to expose adaptive variables for direct 
 cross-jurisdiction multiple-tier leveled stacked side-endpoint complex 
 is enabling shared compartment injunc-
