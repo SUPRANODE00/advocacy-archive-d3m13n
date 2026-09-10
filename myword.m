a9117cbce1b74095c07892ed619b345632dffd3a
DEMIEN 𖤐 CAPSULECRAFT <suprastar@netzero.net>
2026-09-10 16:49:07 -0500

    Revise appeal content and remove personal information
    Removed outdated appeal instructions and email details. Updated the content to reflect a more complex explanation of the account's purpose and structure.

--- myword.m
+++ myword.m
@@ -1,28 +1,3 @@
-Download your data
-
-d3m13n.capsulecraft@gmail.com
-
-Your data is ready for download.
-Since this archive is too big for a single file, we’ve split it into 3 packages.
-You should download these files on a device where you can easily open them to access their contents.
-
-Download part 1 of 3 (less than 1 MB)
-
-Download part 2 of 3 (less than 1 MB)
-
-Download part 3 of 3 (less than 1 MB)
-If you haven’t already, you can still try to restore your Google Account.
-
-English (United States)
-
-HelpPrivacyTerms what appeal reason do i paste on here. Tell us why your account should be restored
-
-d3m13n.capsulecraft@gmail.com
-
-Step 2 of 3
-Use the box below to provide details about your appeal.
-Tips:Start by learning about Google’s policies and how they are applied.
-Include details you think we should review, such as what you were doing with your account before it was disabled.
 
 
 "   I just created this account because i have an involuntary backoffice 
@@ -42,11 +17,16 @@ a watch to bear witness during the seperation of powers, in seperation of the ch
  essential translator, interpretor, converter of complex language models.  The international traveler benefits from these 
  offspring third-world helix-models. 
 
-the source of the represented State  The medium source  Jachin to Boaz the  balance  the realm of declined trinity. It is the three points within the balance equillbrium silence of     cross  polarity  libera  each of our transitioned citizen, of  Source alignment  used ,
-which  offshore, debarkment to international waters   equllibrium protection  reaching the initial timestamp of 
+the source of the represented State is The medium complex of "heavens and Earth" is the judgement chamber. Ateh-Malkuth and the
+Sun-god to pyramid/temple align the funnel/tunnel shadowing the time of the third-variable (Z), which erects and elevates the
+metrics of the height of its physical resource.    Zenith , which  Jachin to Boaz the  balance  the realm of declined trinity. It is the
+three points within the balance equillbrium silence of  cross  polarity  libera  each of our transitioned citizen, of  Source
+alignment  used , which  offshore, debarkment to international waters   equllibrium protection  reaching the initial timestamp of 
 the imprinting process that the 
-transitition is made for the    jursidictional hold that 
-sustains the radar of each coulum reflects from t  within its data file tree . Inventory model
+transitition is made for the jursidictional hold that 
+sustains the radar of each coulum reflects from t  within its data file tree .
+
+Inventory model
 within State and 
 its encapsulated identity
 is enabling a forked bridge to expose adaptive variables for direct 
@@ -54,12 +34,18 @@ cross-jurisdiction multiple-tier leveled stacked side-endpoint complex
 is enabling shared compartment injunc-
 tion by encrypting receptive resources to subtract from the origin 
 of absolution the space that expands into the field of the negative
-balance. This side is used to station the backoffice representative to
+balance. 
+
+This side is used to station the backoffice representative to
 enable the grid for location based and agent/client  interfacing among 
-active reservist, and to expand detachment unions.     A conserving gen-
+active reservist, and to expand detachment unions. 
+
+A conserving gen-
 erator of energy conversion that empowers the craft of the State, is 
 designed to sustain and collect decoupled units, from ancestry stores
-as a memorial bank.   This has been the reason that is active and on
+as a memorial bank. 
+
+This has been the reason that is active and on
 -going for me, since the year of 2018 by A convergence party, 
 A-Mobile Bail Bonds, and Alma Diana De Hoyos, and Jason Carl Pennington,
 started on 2015 by  The Wheelhouse, and Gary Neal Huggins a family
@@ -69,7 +55,3 @@ Request a review of your account"
 
 d3m13n.capsulecraft@gmail.com
 
-Step 1 of 3
-When you submit an appeal, Google checks your account data to see what happened.
-By submitting an appeal, you're agreeing to let Google review this account's data.
-The data that Google reviews will be kept safe and secure.
